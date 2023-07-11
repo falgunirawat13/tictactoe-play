b94c1417a99e0d9a894b4db6c004ba4c624d9fbc
falguni <rawatfalguni13@gmail.com>
2023-07-11 15:48:01 +0530

    included useState hooks to update counter state function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import {  useState } from 'react';
+
 import './styles.scss';
 import Board from './Components/Board';
 
 function App() {
+
+
+const [counter  , setCounter] =  useState(1);
+
+
+console.log("hello");
+
+const onBtnClick = () => {
+
+
+  setCounter(currentCounter => {
+ return currentCounter  + 1;
+  });
+
+};
+
   return( 
    <div className="app">
-    <Board />
+    <div>
+      <button onClick={onBtnClick} >
+        Click me please
+      </button>
+      <div>{counter}</div>
+    </div>
    </div>
   );
 }
